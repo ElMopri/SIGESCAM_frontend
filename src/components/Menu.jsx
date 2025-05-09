3eb581a05f6f2550722e92af4adbb315b1564678
Michelle839 <michellealexandrard@ufps.edu.co>
2025-05-08 23:16:17 -0500

    Arreglo ruta

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,29 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Button from "./Button";
 import "./Menu.css";
-import {IoLogOut} from "react-icons/io5"
+import { IoLogOut } from "react-icons/io5";
 
 const Menu = ({ menuItems, role, handleLogout, isOpen }) => {
   return (
     <div className={`menu-container ${isOpen ? "open" : "closed"}`}>
       <div className={`${role}-menu`}>
         <div className={`${role}-profile`}>
-
-        <img
-          src="/logo.jpg"
-          alt="Logo"
-          className="menu-logo"
-        />
-
+          <img src="/logo.png" alt="Logo" className="menu-logo" />
           <h2 className="nombre">
             {role === "admin" ? "Administrador" : "Gestor de Ventas"}
           </h2>
-          
         </div>
         <nav className="menu">
           {menuItems.map((item) => (
             <Link to={item.path} key={item.text}>
-              <Button text={item.text}  icon={item.icon}/>
+              <Button text={item.text} icon={item.icon} />
             </Link>
           ))}
         </nav>
