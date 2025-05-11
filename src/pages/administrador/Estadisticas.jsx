fb859b8131df498688224f94108c3557095ae89d
Michelle839 <michellealexandrard@ufps.edu.co>
2025-05-11 10:24:58 -0500

    Nuevas Rutas

---
+++ src/pages/administrador/Estadisticas.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Estadisticas = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Parte Estadísticas</h2>
+      <p>Aquí irá la visualización de los datos estadísticos del sistema.</p>
+    </div>
+  );
+};
+
+export default Estadisticas;
